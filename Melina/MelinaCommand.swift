fe6f1a80269f8db9a76851bfc9fa697166e98f96
Aleksey Yakimenko <aleksey.yakimenko@agoda.com>
2022-10-18 11:13:44 +0700

    Add ParsableCommand dependency. Add MelinaCommand.

---
+++ Melina/MelinaCommand.swift
@@ -0,0 +1,25 @@
+//
+//  GenerateCommand.swift
+//  Melina
+//
+//  Created by Aleksey Yakimenko on 18/10/22.
+//
+
+import ArgumentParser
+
+struct Melina: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "melina",
+        abstract: "Code generator for UITests."
+    )
+
+    @Option(
+        name: [.customShort("p"), .customLong("path")],
+        help: "The path to the test specification."
+    )
+    var path: String
+
+    mutating func run() throws {
+       print(path)
+    }
+}
--- Melina/main.swift
+++ Melina/main.swift
@@ -5,7 +5,4 @@
 //  Created by Aleksey Yakimenko on 18/10/22.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+Melina.main()
